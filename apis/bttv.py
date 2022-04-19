f04469d7e8e22cd3b009d8148b5251c1890ae2d7
diego411 <diego.enrico@directbox.com>
2022-04-19 17:14:48 +0200

    additionally fetch emote name

--- apis/bttv.py
+++ apis/bttv.py
@@ -1,6 +1,6 @@
 from matplotlib.style import use
 import requests
-from twitch import get_user_id
+from apis import twitch
 
 CHANNEL_EMOTES_URL = "https://api.betterttv.net/3/cached/users/twitch/"
 EMOTE_URL = "https://cdn.betterttv.net/emote/"
@@ -10,33 +10,27 @@ def fetch_image_link_for_emote(emote):
     print("todo")
 
 
-def fetch_all_image_links_for_channel(channel):
-    user_id = get_user_id(channel)
+def fetch_all_emotes_for_channel(channel):
+    user_id = twitch.get_user_id(channel)
     if not user_id:
         return []
     response = requests.get(CHANNEL_EMOTES_URL + user_id)
     if response.ok:
-        emote_ids = []
+        emotes = []
         channel_emotes = response.json()['channelEmotes']
         shared_emotes = response.json()['sharedEmotes']
 
-        for emote in channel_emotes:
-            emote_ids.append(emote['id'])
-        for emote in shared_emotes:
-            emote_ids.append(emote['id'])
+        for emote in channel_emotes + shared_emotes:
+            emote_id = emote['id']
+            emotes.append({
+                "name": emote['code'],
+                "image_link": image_link_for_emote_id(emote_id)
+            })
 
-        image_links = []
-
-        for emote_id in emote_ids:
-            image_links.append(image_link_for_emote_id(emote_id))
-
-        return image_links
+        return emotes
     else:
         return []
 
 
 def image_link_for_emote_id(emote_id):
     return "https://cdn.betterttv.net/emote/" + str(emote_id) + "/3x"
-
-
-print(fetch_all_image_links_for_channel("nymn"))
