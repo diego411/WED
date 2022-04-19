bcb2579a3cf1438c5ab17148ae9b65ace8c3c9f6
diego411 <diego.enrico@directbox.com>
2022-04-20 00:08:54 +0200

    cache scores in db

--- initial_caching.py
+++ initial_caching.py
@@ -1,3 +1,4 @@
+import json
 import os
 import redis
 import requests
@@ -20,11 +21,30 @@ def download_emote(image_link, path, emote_name):
     file.close()
 
 
-for channel in channels:
-    emotes = bttv.fetch_all_emotes_for_channel(channel)
-    for emote in emotes:
+def cache_emote_score(channel, emote_name, score):
+    cache_for_channel = r.get(channel)
+    cache = {}
+
+    if cache_for_channel:
+        cache = json.loads(cache_for_channel)
+    cache[emote_name] = score.item()
+    r.set(channel, json.dumps(cache))
+
+
+def run():
+    if not os.path.exists(ROOT_FOLDER + "/emotes/"):
+        os.mkdir(ROOT_FOLDER + "/emotes/")
+
+    for channel in channels:
+        emotes = bttv.fetch_all_emotes_for_channel(channel)
         path = ROOT_FOLDER + "/emotes/" + channel + "/"
-        download_emote(emote['image_link'], path, emote['name'])
-        score = query_model.get_weeb_score(path + "/" + emote['name'] + ".png")
-        print(emote['name'])
-        print(score)
+        for emote in emotes:
+            download_emote(emote['image_link'], path, emote['name'])
+            path_to_emote = path + "/" + emote['name'] + ".png"
+            score = query_model.get_weeb_score(path_to_emote)
+            cache_emote_score(channel, emote['name'], score)
+            print(emote['name'])
+            print(score)
+            os.remove(path_to_emote)
+        if os.path.exists(path):
+            os.rmdir(path)
