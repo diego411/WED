563cf1abe6dc57383b227acce930db7fcd555a0c
diego411 <diego.enrico@directbox.com>
2022-04-20 13:39:24 +0200

    add cashing for global bttv emotes

--- apis/bttv.py
+++ apis/bttv.py
@@ -3,10 +3,30 @@ from apis import twitch
 
 CHANNEL_EMOTES_URL = "https://api.betterttv.net/3/cached/users/twitch/"
 EMOTE_URL = "https://cdn.betterttv.net/emote/"
+GLOBAL_EMOTES_URL = "https://api.betterttv.net/3/cached/emotes/global"
 
 
 def fetch_image_link_for_emote(emote):
     print("todo")
+
+
+def fetch_all_global_emotes():
+    response = requests.get(GLOBAL_EMOTES_URL)
+
+    if response.ok:
+        emotes = []
+
+        global_emotes = response.json()
+
+        for emote in global_emotes:
+            emote_id = emote['id']
+            emotes.append({
+                "name": emote['code'],
+                "image_link": image_link_for_emote_id(emote_id)
+            })
+
+        return emotes
+    return []
 
 
 def fetch_all_emotes_for_channel(channel):
--- initial_caching.py
+++ initial_caching.py
@@ -1,5 +1,7 @@
 import json
 import os
+import unicodedata
+import re
 import redis
 import requests
 from apis import bttv
@@ -23,6 +25,17 @@ def download_emote(image_link, path, emote_name):
     file.close()
 
 
+def slugify(value, allow_unicode=False):
+    value = str(value)
+    if allow_unicode:
+        value = unicodedata.normalize('NFKC', value)
+    else:
+        value = unicodedata.normalize('NFKD', value).encode(
+            'ascii', 'ignore').decode('ascii')
+    value = re.sub(r'[^\w\s-]', '', value.lower())
+    return re.sub(r'[-\s]+', '-', value).strip('-_')
+
+
 def cache_emote_score(channel, emote_name, score):
     cache_for_channel = r.get(channel)
     cache = {}
@@ -32,27 +45,44 @@ def cache_emote_score(channel, emote_name, score):
     cache[emote_name] = score.item()
     r.set(channel, json.dumps(cache))
     print("Cached emote " + emote_name + " with score " +
-          str(score) + " for" + "[#" + channel + "]")
+          str(score) + " for " + "[#" + channel + "]")
 
 
-def init_cache(channel, emote_provider):
-    emotes = emote_provider.fetch_all_emotes_for_channel(channel)
-    path = ROOT_FOLDER + "/emotes/" + channel + "/"
+def fetch_and_cache(context, emote_provider):
+    if context == "global":
+        emotes = emote_provider.fetch_all_global_emotes()
+    else:
+        emotes = emote_provider.fetch_all_emotes_for_channel(context)
+
+    path = ROOT_FOLDER + "/emotes/" + context + "/"
     for emote in emotes:
-        download_emote(emote['image_link'], path, emote['name'])
-        path_to_emote = path + "/" + emote['name'] + ".png"
+        path_to_emote = path + slugify(emote['name']) + ".png"
+        download_emote(emote['image_link'], path, slugify(emote['name']))
         score = query_model.get_weeb_score(path_to_emote)
-        cache_emote_score(channel, emote['name'], score)
+        cache_emote_score(context, emote['name'], score)
         os.remove(path_to_emote)
     if os.path.exists(path):
         os.rmdir(path)
 
 
-# def run():
-if not os.path.exists(ROOT_FOLDER + "/emotes/"):
-    os.mkdir(ROOT_FOLDER + "/emotes/")
+def init_third_party_emote_cache():
+    for channel in channels:
+        fetch_and_cache(channel, bttv)
+        fetch_and_cache(channel, ffz)
+        fetch_and_cache(channel, seventv)
 
-for channel in channels:
-    init_cache(channel, bttv)
-    init_cache(channel, ffz)
-    init_cache(channel, seventv)
+
+def init_global_emote_cache():
+    fetch_and_cache("global", bttv)
+
+
+def run():
+    if not os.path.exists(ROOT_FOLDER + "/emotes/"):
+        os.mkdir(ROOT_FOLDER + "/emotes/")
+
+    init_third_party_emote_cache()
+
+    if not os.path.exists(ROOT_FOLDER + "/emotes/"):
+        os.mkdir(ROOT_FOLDER + "/emotes/")
+
+    init_global_emote_cache()
